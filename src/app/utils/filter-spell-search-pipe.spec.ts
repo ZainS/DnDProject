d33041b59ac7bc9ad20b1f43e034e9c3e253c9eb
ZainS <zain71787@gmail.com>
2025-10-26 01:15:26 -0400

    Added Search by name and level functionality. Proper pipes added.

---
+++ src/app/utils/filter-spell-search-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterSpellSearchPipe } from './filter-spell-search-pipe';
+
+describe('FilterSearchPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterSpellSearchPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
