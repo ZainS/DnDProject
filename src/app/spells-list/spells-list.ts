704fdd059fd6f5237779758c47deb2abecd1c8aa
ZainS <zain71787@gmail.com>
2025-10-26 01:12:00 -0400

    Added Search by name and level functionality.

--- src/app/spells-list/spells-list.ts
+++ src/app/spells-list/spells-list.ts
@@ -5,7 +5,8 @@ import {MatProgressSpinner} from '@angular/material/progress-spinner';
 import {MatFormField} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatInput, MatLabel} from '@angular/material/input';
-import {FilterSpellSearchPipe} from '../utils/filter-search-pipe';
+import {FilterSpellSearchPipe} from '../utils/filter-spell-search-pipe';
+import {MatOption, MatSelect, MatSelectChange} from '@angular/material/select';
 
 @Component({
   selector: 'app-spells-list',
@@ -16,7 +17,10 @@ import {FilterSpellSearchPipe} from '../utils/filter-search-pipe';
     FormsModule,
     MatInput,
     MatLabel,
-    FilterSpellSearchPipe
+    FilterSpellSearchPipe,
+    MatSelect,
+    MatOption,
+    MatOption,
   ],
   templateUrl: './spells-list.html',
   styleUrl: './spells-list.css'
@@ -24,16 +28,28 @@ import {FilterSpellSearchPipe} from '../utils/filter-search-pipe';
 export class SpellsList {
 
   spellService = inject(SpellsApi)
+  fullSpellList: any[] | undefined;
   spellList: any[] | undefined;
   searchSpell: string = '';
   loading: boolean;
+  selectedLevels: number[] = [];
 
   constructor() {
     this.loading = true;
     this.spellService.getAllSpellDetails().subscribe((results: any) => {
-      this.spellList = results;
+      this.fullSpellList = results;
+      this.spellList = this.fullSpellList;
       this.loading = false;
     });
+  }
+
+  getSpellByLevel(levels: MatSelectChange) {
+    if (this.selectedLevels.length === 0) {
+      this.spellList = this.fullSpellList;
+      return this.spellList;
+    }
+    this.spellList = this.fullSpellList?.filter(spell => this.selectedLevels.includes(spell.level));
+    return this.spellList;
   }
 
 }
